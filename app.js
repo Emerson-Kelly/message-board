11aa794ceb17c384ad4cc70bbe855b0daf673663
Emerson Kelly <123923271+Emerson-Kelly@users.noreply.github.com>
2025-04-08 22:34:59 -0400

    Configure EJS As The Templating Engine With Bootstrap Styles

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-import express from 'express'
+import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
 
-const app = express()
+const app = express();
+
+// Wmulate __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+    res.render('index');
+});
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
