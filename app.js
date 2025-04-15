7b3fc19565a197e91b71ae0702f2ff9b4d7a7bdb
Emerson Kelly <123923271+Emerson-Kelly@users.noreply.github.com>
2025-04-14 22:08:17 -0400

    Ensure App is Listening on process.env.PORT for Railway Deployment

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/', indexRouter);
 app.use('/new', messageRouter);
 app.use('/', detailsRouter);
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
