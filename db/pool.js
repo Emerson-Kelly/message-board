ad3ba4fa96d1fb371ecd08f4d4bafeacc048cf9c
Emerson Kelly <123923271+Emerson-Kelly@users.noreply.github.com>
2025-04-22 23:20:53 -0400

    Update pool and queries file for production

--- db/pool.js
+++ db/pool.js
@@ -5,15 +5,10 @@ dotenv.config();
 import pkg from 'pg';
 const { Pool } = pkg;
 
-//console.log(process.env);
-
 const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } 
 });
 
 export default pool;
+
